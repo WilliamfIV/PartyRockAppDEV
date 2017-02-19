59f5fa67bdf5d9c397e0b1f1487845cd4c286c2f
William Fulton <will.fulton95@gmail.com>
2017-02-19 01:03:20 -0500

    Added images, added vied, added table view, added table cell, added second ViewController, added segue between MainVC to VideoVC, added web link to the "VideoVC"

---
+++ PartyRockAppDEV/MainVC.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  PartyRockAppDEV
+//
+//  Created by William H Fulton IV on 2/18/17.
+//  Copyright © 2017 IVdevelopment. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+}
+
+}
